701920a5e8d2d015b7c53006b59c55770dd4f732
piniass <miguelpi1998@gmail.com>
2023-12-14 12:29:13 +0100

    Base de datos

---
+++ paises.py
@@ -0,0 +1,37 @@
+from database import db
+from sqlalchemy.sql import func
+from datetime import date
+
+# Para crear las tablas, desde el entorno de ejecución de Python, ejecutar:
+# from database import app, db
+# from estudiante import Estudiante
+# app.app_context().push()
+# db.create_all()
+
+class Pais(db.Model):
+    
+    __tablename__ = 'paises'
+#falta terminar esto     
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    capital = db.Column(db.String(100), nullable=False)
+    bandera_url = db.Column(db.String(100), nullable=False)
+    bandera_blob = db.Column(db.LargeBinary, nullable=False)
+    habitantes = db.Column(db.Integer, nullable=False)
+    diaNacional = db.Column(db.Date, nullable=False)
+    
+     
+    def __init__(self, nombre,edad, capital, bandera_url, bandera_blob, habitantes, diaNacional):
+        self.nombre = nombre
+        self.edad = edad
+        self.capital = capital
+        self.bandera_url = bandera_url
+        self.bandera_blob = bandera_blob
+        self.habitantes = habitantes
+        self.diaNacional = diaNacional
+
+
+
+
+    def __repr__(self):
+        return f'<Pais {self.id}>: {self.nombre}, {self.capital}, {self.bandera_url}, {self.bandera_blob}, {self.habitantes}, {self.diaNacional}'
